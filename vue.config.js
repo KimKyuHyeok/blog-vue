7ab6696905cf9d7b3ce5310670b1c22707a16e7a
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-07-25 14:13:58 +0900

    feat: 초기 셋팅

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/': {
+        target: 'http://localhost:80',
+        changeOrigin: true,
+        pathRewrite: { '^/': '' },
+        ws: false
+      },
+    }
+  }
+};
