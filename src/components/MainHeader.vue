a69b954d8c69643accac7833607f25b60fd141fe
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-04-30 15:50:17 +0900

    feat: 헤더 컴포넌트 작성

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <header id="header">
+        <div class="logo" @click="goToMainPage">DevKyu96</div>
+
+        <a class="admin-link" href="/admin">admin</a>
+    </header>
+    
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter()
+
+const goToMainPage = () => {
+    router.push({ path: '/' })
+}
+</script>
+
+
+<style scoped>
+#header {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
+}
+
+.logo {
+    font-size: 30px;
+    margin-left: 200px;
+    cursor: pointer;
+}
+
+.admin-link {
+    text-decoration: none;
+    color: darkgray;
+    margin-right: 200px;
+}
+
+</style>
