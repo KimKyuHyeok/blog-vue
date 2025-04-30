8bae9f165e5768d130a3c5c2ce0988ac5e11f60b
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-04-30 15:57:38 +0900

    feat: admin Post 관련 페이지 작성

---
+++ src/views/admin/AdminPost.vue
@@ -0,0 +1,26 @@
+<script>
+import AdminPost from '@/components/admin/PostEditor.vue'
+
+
+export default {
+    name: 'AdminPost',
+    components: {
+        AdminPost
+    }
+}
+</script>
+
+<template>
+    <div class="center-container">
+        <AdminPost/>
+    </div>
+</template>
+
+<style scoped>
+.center-container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
